da214165b6814de02916a4a4e66f7ab2334d54ec
ZIHIKA <1918938521@qq.com>
2025-08-21 11:06:54 +0800

    UserController初步实现register

--- src/main/java/org/example/flingblogs/controller/UserController.java
+++ src/main/java/org/example/flingblogs/controller/UserController.java
@@ -1,4 +1,27 @@
 package org.example.flingblogs.controller;
 
+
+import org.example.flingblogs.model.User;
+import org.example.flingblogs.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public User register(@RequestBody User user){
+        // TODO: 加密, 校验
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> getUser(@PathVariable Long id){
+        return userRepository.findById(id);
+    }
 }
