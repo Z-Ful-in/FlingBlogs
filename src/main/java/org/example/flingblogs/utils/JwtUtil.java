e4bf614bd61190540b70534573825b2c0212d72e
ZIHIKA <1918938521@qq.com>
2025-08-22 10:48:08 +0800

    Jwt登录认证工具

---
+++ src/main/java/org/example/flingblogs/utils/JwtUtil.java
@@ -0,0 +1,46 @@
+package org.example.flingblogs.utils;
+
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private static final String SECRET = "ziheikuangziheikuangziheikuangziheikuang";
+    private static final long EXPIRATION = 1000 * 60 * 60;
+
+    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public String generateToken(String username){
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public Boolean validateToken(String token){
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException e){
+            return false;
+        }
+    }
+}
